bd830d5058cdadccd80d63e62c9e739e233f4724
Daniil <dadyarri@gmail.com>
2023-04-23 00:45:52 +0300

    refactor: reorder files

--- AreaCalculation/Interfaces/IFigureInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AreaCalculation.Interfaces;
-
-/// <summary>
-/// Interface to store information, required to calculate area of a figure
-/// </summary>
-public interface IFigureInfo
-{
-}
