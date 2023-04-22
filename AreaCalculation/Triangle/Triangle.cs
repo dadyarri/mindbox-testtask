eb29a0cfd5012cfd20ff214bada3a0d89c0aff11
Daniil <dadyarri@gmail.com>
2023-04-22 23:16:23 +0300

    feat: add implementation for triangle

---
+++ AreaCalculation/Triangle/Triangle.cs
@@ -0,0 +1,42 @@
+﻿using AreaCalculation.Interfaces;
+
+namespace AreaCalculation.Triangle;
+
+public class Triangle : IFigure<TriangleInfo>
+{
+    private readonly TriangleInfo _figureInfo;
+
+    public Triangle(TriangleInfo figureInfo)
+    {
+        _figureInfo = figureInfo;
+    }
+
+    public double CalculateArea()
+    {
+        if (IsRightTriangle())
+        {
+            var cathets = new List<double> { _figureInfo.SideA, _figureInfo.SideB, _figureInfo.SideC }
+                .OrderBy(s => s).Take(2).ToList();
+            
+            return (cathets[0] * cathets[1]) / 2;
+        }
+
+        var perimeter = _figureInfo.SideA + _figureInfo.SideB + _figureInfo.SideC;
+        var halfMeter = perimeter / 2;
+
+        return Math.Sqrt(halfMeter * (halfMeter - _figureInfo.SideA) * (halfMeter - _figureInfo.SideB) *
+                         (halfMeter - _figureInfo.SideC));
+    }
+
+    private bool IsRightTriangle()
+    {
+        return (
+            (_figureInfo.SideA * _figureInfo.SideA + _figureInfo.SideB * _figureInfo.SideB ==
+             _figureInfo.SideC * _figureInfo.SideC) ||
+            (_figureInfo.SideA * _figureInfo.SideA + _figureInfo.SideC * _figureInfo.SideC ==
+             _figureInfo.SideB * _figureInfo.SideB) ||
+            (_figureInfo.SideC * _figureInfo.SideC + _figureInfo.SideB * _figureInfo.SideB ==
+             _figureInfo.SideA * _figureInfo.SideA)
+        );
+    }
+}
---
+++ AreaCalculation/Triangle/TriangleInfo.cs
@@ -0,0 +1,10 @@
+﻿using AreaCalculation.Interfaces;
+
+namespace AreaCalculation.Triangle;
+
+public class TriangleInfo: IFigureInfo
+{
+    public double SideA { get; set; }
+    public double SideB { get; set; }
+    public double SideC { get; set; }
+}
